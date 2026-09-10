d887341573c6474ad572bc4821d3ff37308178bb
arkabumigroup <arkabumigroup@gmail.com>
2026-09-10 12:57:52 +0700

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -9,7 +9,7 @@ export default {
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'nav-bar-content-after': () => h('img', {
-        src: '/src/Logo-Light.png',
+        src: 'src/Logo-Light.png',
         alt: 'Logo',
         class: 'navbar-extra-logo'
       })
